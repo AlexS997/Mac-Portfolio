ccaf664d85f08d55d47aa21f53dba9b47bd3eab9
AlexS997 <alexandru.solomon@yahoo.com>
2025-11-25 08:54:01 +0100

    Implement Dock and Welcome

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Dock from "#components/Dock"
 import Navbar from "#components/Navbar"
+import Welcome from "#components/Welcome"
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Welcome />
+      <Dock />
     </main>
   )
 }
